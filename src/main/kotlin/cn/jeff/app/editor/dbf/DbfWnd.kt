eff6ccfb334e667ac3c3c470456dc927da3274b4
Jeff <zhsu@amwares.com>
2021-08-10 18:39:48 +0800

    表格难以参数化，标签页也有不少问题，不知道怎么用。明天再继续。

--- src/main/kotlin/cn/jeff/app/editor/dbf/DbfWnd.kt
+++ src/main/kotlin/cn/jeff/app/editor/dbf/DbfWnd.kt
@@ -1,10 +1,54 @@
 package cn.jeff.app.editor.dbf
 
 import tornadofx.*
+import kotlin.reflect.KFunction
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty1
 
-class DbfWnd : View("My View") {
+/*
+class DbfWnd<T>(data: List<T>, colList: List<Pair<String, KFunction<T>>>) : View() {
 
-	override val root = borderpane {
+	override val root = tableview(data.observable()) {
+		colList.forEach { (title, getter) ->
+			column(title, getter)
+		}
+		refresh()
+	}
+
+//	override val root = datagrid(data) {
+//	}
+
+	override fun onDelete() {
+		super.onDelete()
+		println("删除窗口。")
+	}
+
+	override fun onUndock() {
+		super.onUndock()
+		print("离开！")
+	}
+
+}
+*/
+
+class DbfWnd(data: List<Pair<Int, String>>) : View() {
+
+	override val root = tableview(data.observable()) {
+		readonlyColumn("序号", Pair<Int, String>::first)
+		readonlyColumn("内容", Pair<Int, String>::second)
+	}
+
+//	override val root = datagrid(data) {
+//	}
+
+	override fun onDelete() {
+		super.onDelete()
+		println("删除窗口。")
+	}
+
+	override fun onUndock() {
+		super.onUndock()
+		print("离开！")
 	}
 
 }
--- src/main/kotlin/cn/jeff/app/editor/dbf/MainWnd.kt
+++ src/main/kotlin/cn/jeff/app/editor/dbf/MainWnd.kt
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
 import javafx.scene.layout.BorderPane
 import tornadofx.*
+import kotlin.random.Random
 
 class MainWnd : View("DBF编辑器") {
 
@@ -26,7 +27,11 @@ class MainWnd : View("DBF编辑器") {
 	fun btnClicked(actionEvent: ActionEvent) {
 		when (actionEvent.source) {
 			j.btnOpenFile -> {
-				j.lbStatus.text = "很好！"
+				j.lbStatus.text = "运行中……"
+				val demoData = listOf(1 to "one", 2 to "two", 3 to "three")
+				val dbfWnd = DbfWnd(demoData)
+				j.mainTabPane.tab(dbfWnd).text = "d${Random.nextInt(9999)}"
+				j.lbStatus.text = "就绪"
 			}
 			else -> {
 				println("不会运行到这里。")
