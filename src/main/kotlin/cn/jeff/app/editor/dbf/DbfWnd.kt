3b81237816b0e5a6680791fb8123241ffb87164f
Jeff <zhsu@amwares.com>
2021-08-26 10:37:28 +0800

    去掉无用的注释。

--- src/main/kotlin/cn/jeff/app/editor/dbf/DbfWnd.kt
+++ src/main/kotlin/cn/jeff/app/editor/dbf/DbfWnd.kt
@@ -24,7 +24,6 @@ class DbfWnd(private val dbfFilename: String) : Fragment() {
 	private val defaultCharset = Charset.forName(GlobalVars.appConf.defaultCharset)
 
 	override val root = borderpane {
-		// println(CaigenDataSource())
 		val (fields, records) = loadDbf()
 		top {
 			hbox {
@@ -137,42 +136,3 @@ class DbfWnd(private val dbfFilename: String) : Fragment() {
 	}
 
 }
-
-/*
-class DbfWnd(titles: Array<String>, data: ObservableList<Array<Any>>) : Fragment() {
-
-	override val root = tableview(data) {
-		titles.forEachIndexed { i, title ->
-			column<Array<Any>, Any>(title) {
-				val arrAccessor = ArrAccessor(it.value, i)
-//				it.value.observable(
-//					getter = arrAccessor::value::get,
-//					setter = arrAccessor::value::set
-//				)
-
-				arrAccessor.observable("value")
-				// arrAccessor.observable(ArrAccessor::value)	// 这句也可以。
-			}.makeEditable(object : StringConverter<Any>() {
-				override fun toString(obj: Any?): String =
-					obj.toString()
-
-				override fun fromString(str: String?): Any {
-					return str ?: ""
-				}
-			})
-		}
-	}
-
-	class ArrAccessor(private val arr: Array<Any>, private val ind: Int) {
-		var value: Any
-			get() = arr[ind]
-			set(value) {
-				arr[ind] = value
-//				arr.forEach {
-//					println("$it : ${it.javaClass}")
-//				}
-			}
-	}
-
-}
-*/
